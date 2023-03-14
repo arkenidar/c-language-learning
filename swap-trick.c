1c1b9a8a60362e71bc0107828d6543974d4c38a7
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-03-14 23:25:33 +0100

    clean-up and minimization

--- swap-trick.c
+++ swap-trick.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-
-struct pair{ int x,y; }; // define "pair"
-struct pair swap(struct pair from){ // not necessarily defined (one life suffices)
-    struct pair to={ .x=from.y, .y=from.x }; // init "to" pair
+typedef struct _pair{ int x,y; } pair; // define "pair"
+pair swap(pair from){ // not necessarily defined (one life suffices)
+    pair to={ .x=from.y, .y=from.x }; // init "to" pair
     return to;
 }
-
 int main(){
-
-    struct pair from={ .x=1, .y=2 }; // init "from" pair
-    struct pair to = swap(from); // swap and assign
+    pair to = swap((pair){ .x=1, .y=2 }); // swap and assign
     printf(" to: x=%d; y=%d \n", to.x, to.y); // print results
     return 0;
 }
-
-/*
-$ ./swap-trick 
- to: x=2; y=1 
-*/
