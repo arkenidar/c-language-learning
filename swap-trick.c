fc590c40faac34074746de421c6ebbedfd247fff
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-02-23 15:17:32 +0100

    format (this too)

--- swap-trick.c
+++ swap-trick.c
@@ -1,11 +1,19 @@
+// gcc swap-trick.c -o swap-trick
 
-typedef struct _pair{ int x,y; } pair;
-pair swap(pair from){ return (pair){ .x=from.y, .y=from.x }; }
+typedef struct _pair
+{
+    int x, y;
+} pair;
+pair swap(pair from)
+{
+    return (pair){.x = from.y, .y = from.x};
+}
 
 #include <stdio.h>
-int main(){
+int main()
+{
 
-    pair to = swap((pair){ .x=1, .y=2 });
+    pair to = swap((pair){.x = 1, .y = 2});
     printf(" to: x=%d; y=%d \n", to.x, to.y);
 
     return 0;
