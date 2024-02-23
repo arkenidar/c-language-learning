f115122cc1a38ada839f9134be000b26a59199ee
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-02-23 15:21:02 +0100

    format again (re-discovering this code-base)

--- for.c
+++ for.c
@@ -1,19 +1,26 @@
+// gcc for.c -o for
 
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    
-    for(int i=0; i<4; i++) printf("%d ",i);
-    
-    puts(""); puts("--------------");
-    
-    for(int i=0; i<4; i++) printf("%d ", i+1 );
-    
-    puts(""); puts("--------------");
+int main()
+{
 
-    for(int i=1; i<=4; i++) printf("%d ",i);
-    
-    puts(""); puts("--------------");
+    for (int i = 0; i < 4; i++)
+        printf("%d ", i);
 
+    puts("");
+    puts("--------------");
+
+    for (int i = 0; i < 4; i++)
+        printf("%d ", i + 1);
+
+    puts("");
+    puts("--------------");
+
+    for (int i = 1; i <= 4; i++)
+        printf("%d ", i);
+
+    puts("");
+    puts("--------------");
 }
